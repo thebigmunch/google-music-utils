3cd2eafa77451d6f3c49408ef60814b8bc15fa28
thebigmunch <mail@thebigmunch.me>
2019-08-20 19:48:02 -0400

    Add missing nox sessions

--- noxfile.py
+++ noxfile.py
@@ -13,7 +13,7 @@ ON_TRAVIS = 'TRAVIS' in os.environ
 @nox.session(python=py37)
 def lint(session):
 	session.install('.[lint]')
-	session.run('flake8', 'src/')
+	session.run('flake8', 'src/', 'tests/')
 
 
 @nox.session(python=py37)
@@ -31,3 +31,28 @@ def doc(session):
 		'.',
 		'_build/html'
 	)
+
+
+@nox.session(python=[py36, py37, py38])
+def test(session):
+	session.notify('coverage')
+	session.notify('report')
+
+
+@nox.session
+def coverage(session):
+	session.install('.[test]')
+	session.run('coverage', 'run', '-m', 'pytest')
+	session.notify('report')
+
+
+@nox.session
+def report(session):
+	session.install('coverage')
+	session.run('coverage', 'report', '-m')
+
+	if ON_TRAVIS:
+		session.install('codecov')
+		session.run('codecov')
+
+	session.run('coverage', 'erase')
